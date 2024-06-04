47b36478d4fa0ce4f2cf9b6b61e42b0132c60065
ajai <ajaikumarvs@gmail.com>
2024-06-04 14:18:58 +0530

    init

--- server/index.js
+++ server/index.js
@@ -1,2 +1,7 @@
+//standard code
 const express = require('express')
-const app = express()
+const app = express() //initialize server, send API requests etc.
+
+app.listen(3001, () => {
+console.log("Server running on port 3001")
+})
